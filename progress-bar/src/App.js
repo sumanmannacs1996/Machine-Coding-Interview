e181b7c10f70918b29a2d22f53a3690a96ac3311
Suman Manna <sumanmannacs1996@gmail.com>
2025-06-30 22:21:03 +0530

    progress bar update

--- progress-bar/src/App.js
+++ progress-bar/src/App.js
@@ -1,18 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import ProgressBar from "./components/ProgressBar";
 
 function App() {
   const [value, setValue] = useState(0);
   const [success, setSuccess] = useState(false);
+  const timer = useRef(null);
   useEffect(() => {
-    const interval = setInterval(() => {
-      setValue((prev) => prev + 0.1);
-    }, 20);
+    if (value < 100) {
+      timer.current = setTimeout(() => {
+        setValue((prev) => prev + 1);
+      }, 50);
+    } else {
+      clearInterval(timer.current);
+    }
     return () => {
-      clearInterval(interval);
+      clearInterval(timer.current);
     };
-  }, []);
+  }, [value]);
   return (
     <div className="App">
       <span>Progress Bar</span>
--- progress-bar/src/components/ProgressBar.js
+++ progress-bar/src/components/ProgressBar.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 function ProgressBar({ value = 0, onCompleate = () => {} }) {
-  const [percent, setPercent] = useState(value);
   useEffect(() => {
-    setPercent(Math.min(Math.max(value, 0), 100));
-
     if (value >= 100) {
       onCompleate();
     }
@@ -13,12 +10,12 @@ function ProgressBar({ value = 0, onCompleate = () => {} }) {
     <div className="progress">
       <span
         style={{
-          color: percent > 49 ? "white" : "black",
+          color: value > 49 ? "white" : "black",
         }}
       >
-        {percent.toFixed(2)}%
+        {value.toFixed(2)}%
       </span>
-      <div style={{ width: `${percent}%` }}></div>
+      <div style={{ width: `${value}%` }}></div>
     </div>
   );
 }
